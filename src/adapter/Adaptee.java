ea8c2d0e85321e1ed585f47531e455fe8a66ae7f
netzon-eladio <eladio.sareno@netzontech.com>
2021-06-18 15:21:13 +0800

    Adapter Pattern

---
+++ src/adapter/Adaptee.java
@@ -0,0 +1,7 @@
+package adapter;
+
+public class Adaptee {
+    public void requestSpecific() {
+
+    }
+}
---
+++ src/adapter/Main.java
@@ -0,0 +1,8 @@
+package adapter;
+
+public class Main {
+    public static void main(String[] args) {
+        ITarget target = new Adapter(new Adaptee());
+        target.request();
+    }
+}
